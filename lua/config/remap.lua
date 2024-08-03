1e795c34ff98ba420cb2fa7df7d1701eb87acfde
Jonathan Fesseha <jonathan.fesseha@student.manchester.ac.uk>
2024-08-03 21:28:46 +0100

    more key maps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>b", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --
 -- Remap J and K in visual mode to move selection up and down
 vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
@@ -11,3 +11,20 @@ vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
 
+-- don't yank when pasting in a selction, or when deleting
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- yank to clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
+
+-- change all occorunces of current word in file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
