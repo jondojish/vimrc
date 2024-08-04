2deb2af908b26ff3ac3d06a1a488cdd1eaec63e0
Jonathan Fesseha <jonathan.fesseha@student.manchester.ac.uk>
2024-08-05 00:56:11 +0100

    change binds

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,21 +2,21 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --
 -- Remap J and K in visual mode to move selection up and down
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { noremap = true, silent = true })
 -- Key mappings to switch between splits using leader key
-vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- don't yank when pasting in a selction, or when deleting
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- yank to clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "J", "mzJ`z")
